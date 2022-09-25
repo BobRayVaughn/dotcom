ed680a71f48eb73df86e60357bcda20de9e1358e
Bob Ray Vaughn <bobrayvaughn@gmail.com>
2022-09-25 04:26:03 -0500

    Nav IC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Nav } from './nav';
+import { Main } from './pages';
+import NavProvider from './context/NavContext';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="appContainer">
       <header className="App-header">
+      <NavProvider>
+				<Nav />
+				<Main />
+			</NavProvider>
         <p>
           Work in Progress
         </p>
